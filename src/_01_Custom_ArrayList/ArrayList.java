52cb1533b87b67c1871f3af96d7648d28a136e9b
BuildTools <unconfigured@null.spigotmc.org>
2020-09-16 19:59:54 -0700

    enums r everywhere

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,33 +3,64 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
+	T[] arr;
 	
 	public ArrayList() {
+		arr = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		T temp = arr[loc];
+		return temp;
 	}
 	
 	public void add(T val) {
-		
+		T[] temp = (T[]) new Object[arr.length+1];
+		temp[arr.length] = val;
+		for(int i=0;i<arr.length;i++) {
+			temp[i] = arr[i];
+		}
+		arr = temp;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] temp = (T[]) new Object[arr.length+1];
+		for(int i=0;i<loc;i++) {
+			temp[i] = arr[i];
+		}
+		temp[loc] = val;
+		for(int i=loc;i<arr.length;i++) {
+			temp[i+1] = arr[i];
+		}
+		arr = temp;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		arr[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+		T[] temp = (T[]) new Object[arr.length-1];
+		for(int i=0;i<loc;i++) {
+			temp[i] = arr[i];
+		}
+		for(int i=loc;i<temp.length;i++) {
+			temp[i] = arr[i+1];
+		}
+		arr = temp;
 	}
 	
 	public boolean contains(T val) {
-		
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i].equals(val)) {
+				return true;
+			}
+		}
 		return false;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return arr.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,28 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
+		switch(choice) {
+		case "Sunday":
+			System.out.println("It's Sunday.");
+			break;
+		case "Monday":
+			System.out.println("It's Should-Be-A-Break-Day.");
+			break;
+		case "Tuesday":
+			System.out.println("It's Tuesday.");
+			break;
+		case "Wednesday":
+			System.out.println("It's Wednesday.");
+			break;
+		case "Thursday":
+			System.out.println("It's Thursday.");
+			break;
+		case "Friday":
+			System.out.println("IT'S FRIDAY FRIDAY GOTTA GET DOWN ON FRIDAY!!!1!!!!11!!");
+			break;
+		case "Saturday":
+			System.out.println("It's Saturday.");
+			break;
+		}
 	}
 }
